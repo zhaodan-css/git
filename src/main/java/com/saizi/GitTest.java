a57d99cc5a1051c30a8d713c2d09ee859db5ab21
zhaodan-css <58286584+zhaodan-css@users.noreply.github.com>
2021-09-12 15:02:23 +0800

    Update GitTest.java
    2

--- src/main/java/com/saizi/GitTest.java
+++ src/main/java/com/saizi/GitTest.java
@@ -10,6 +10,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hot-fix change");
-        System.out.println("push test");
+        System.out.println("push tes32t");
     }
 }
